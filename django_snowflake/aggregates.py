3cf10ee303ceec8f6860e2d4eb4d80637a445e04
Dylan <dylan@airgarage.com=>
2026-01-14 16:46:29 -0800

    downgrade code

--- django_snowflake/aggregates.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.aggregates import StringAgg
-
-
-def string_agg(self, compiler, connection, **extra_context):
-    return self.as_sql(compiler, connection, function="LISTAGG", **extra_context)
-
-
-def register_aggregates():
-    StringAgg.as_snowflake = string_agg
